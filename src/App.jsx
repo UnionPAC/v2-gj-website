505f5bf9ff2542b2ea7ecaf7c4a9105fe551339a
Geoff Jamieson <49350011+UnionPAC@users.noreply.github.com>
2023-09-29 08:30:00 -0400

    add space bg with tsparticles

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,13 @@ import Hero from "./components/Hero";
 import About from "./components/About";
 import Projects from "./components/Projects";
 import Contact from "./components/Contact";
+import ParticleBg from "./components/ParticleBg";
 
 const App = () => {
   return (
     <>
       <main className="h-[100dvh] snap-y snap-mandatory scroll-smooth overflow-y-auto no-scrollbar">
+        <ParticleBg />
         <Hero />
         <About />
         <Projects />

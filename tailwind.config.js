37b4afb2a33babdc17914ac87b470bbd8c85a08a
Geoff Jamieson <49350011+UnionPAC@users.noreply.github.com>
2023-10-03 19:30:17 -0400

    set screen sizes in tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,17 @@ export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
+    screens: {
+      sm: "640px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1280px",
+      "2xl": "1536px",
+    },
+    fontFamily: {
+      syne: ["Syne", "sans-serif"],
+      inter: ["Inter", "sans-serif"],
+    },
   },
   plugins: [],
 };

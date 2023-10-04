1bce573ffb6661e329cbc03465d11cb70fa01dd8
Geoff Jamieson <49350011+UnionPAC@users.noreply.github.com>
2023-10-04 10:39:37 -0400

    removed unnecessary code

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,13 +3,6 @@ export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
-    screens: {
-      sm: "640px",
-      md: "768px",
-      lg: "1024px",
-      xl: "1280px",
-      "2xl": "1536px",
-    },
     fontFamily: {
       syne: ["Syne", "sans-serif"],
       inter: ["Inter", "sans-serif"],

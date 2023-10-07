3f9e683c98c6c23bdbd78163cf59ad3d62edd3af
Geoff Jamieson <49350011+UnionPAC@users.noreply.github.com>
2023-10-07 14:47:15 -0400

    add custom theme and styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,9 +3,20 @@ export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
-    fontFamily: {
-      syne: ["Syne", "sans-serif"],
-      inter: ["Inter", "sans-serif"],
+    screens: {
+      xsm: "420px",
+      sm: "640px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1280px",
+      "2xl": "1536px",
+    },
+    colors: {
+      white: "#FAF9F5",
+      black: "#050505",
+      primary: "#4E6EB3",
+      secondary: "#141A35",
+      accent: "#9FFF8A",
     },
   },
   plugins: [],
